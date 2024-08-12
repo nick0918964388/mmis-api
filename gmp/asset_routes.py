c3dadea95d79956180426cc95cf494bd7fb094ac
nickyin <nickyin@tw.ibm.com>
2024-08-12 18:23:20 +0800

    add log

--- gmp/asset_routes.py
+++ gmp/asset_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 import os
+import logging
 from db_connection import get_db_connection
 from db_utils import execute_query
 
@@ -9,7 +10,7 @@ gmp_asset_bp = Blueprint('gmp_asset', __name__)
 def query_asset():
     # 紀錄請求的參數
     search_text = request.args.get('search', '')
-    print(f"Received search text: {search_text}")  # 除錯輸出
+    logging.info(f"Received search text: {search_text}")  # 除錯輸出
     
     sql = """
     SELECT * FROM maximo.asset 
@@ -19,6 +20,6 @@ def query_asset():
     result = execute_query(sql, params, limit=10)
     
     # 紀錄結果
-    print(f"Query result: {result}")  # 除錯輸出
+    logging.info(f"Query result: {result}")  # 除錯輸出
     
     return jsonify(result)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+import logging
+from flask import Flask, request
 from mmis.asset_routes import asset_bp
 from mmis.item_routes import item_bp
 from mmis.person_routes import person_bp
@@ -7,7 +8,18 @@ from gmp.asset_routes import gmp_asset_bp
 
 # os.add_dll_directory('./db2cli/clidriver/bin') 
 
+# 設置日誌配置
+logging.basicConfig(
+    filename='app.log',  # 日誌檔案名稱
+    level=logging.INFO,  # 設置日誌級別
+    format='%(asctime)s - %(levelname)s - %(message)s'  # 日誌格式
+)
+
 app = Flask(__name__)
+
+@app.before_request
+def log_request_info():
+    logging.info(f"Request path: {request.path}")  # 記錄請求路徑
 
 app.register_blueprint(asset_bp)
 app.register_blueprint(item_bp)
