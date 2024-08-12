d2b7110fb91b3ff7351a7f1886fc32644bb297d9
nickyin <nickyin@tw.ibm.com>
2024-08-12 17:52:25 +0800

    更新oracle

--- db_connection.py
+++ db_connection.py
@@ -21,8 +21,8 @@ def get_db_connection():
     import cx_Oracle
 
     def connect_to_oracle():
-        dsn = cx_Oracle.makedsn(host, port, service_name=database)  # 使用相同的參數
-        connection = cx_Oracle.connect(user=username, password=password, dsn=dsn)
+        dsn = cx_Oracle.makedsn(host, port, sid=database)  # 使用SID而不是服務名稱
+        connection = cx_Oracle.connect(user=username, password=password, dsn=dsn, mode=cx_Oracle.SYSDBA)  # 使用SYSDBA模式
         return connection
 
     # 根據資料庫類型選擇連接方式
--- gmp/asset_routes.py
+++ gmp/asset_routes.py
@@ -8,11 +8,10 @@ gmp_asset_bp = Blueprint('gmp_asset', __name__)
 
 @gmp_asset_bp.route('/gmp/asset', methods=['GET'])
 def query_asset():
-    search_text = request.args.get('search', '')
+    
     
     sql = """
     SELECT * FROM maximo.asset 
-    WHERE LOWER(description) LIKE ? or LOWER(assetnum) LIKE ?
     """
     params = [f'%{search_text.lower()}%', f'%{search_text.lower()}%']
     
